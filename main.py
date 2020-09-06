4b8dfde6343828fda11d2dd976fadb032383634e
Sofia Borodina <soniakotya@gmail.com>
2020-09-06 19:31:03 +0300

    added main.py file

---
+++ main.py
@@ -0,0 +1,25 @@
+import socket
+import time
+import threading
+import select
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(("0.0.0.0", 8080))
+sock.listen(100)
+
+# epoll = select.epoll
+# epoll.register(conn.fileno(), select.EPOLLIN | select.EPOLLOUT)
+
+def handler(conn):
+    time.sleep(10)
+    conn.send(b"Hellow world\n")
+    conn.close()
+
+while True:
+    conn, address = sock.accept()
+    print("Client address: ", address[0], " Client port: ", address[1])
+    data = conn.recv(1024)
+    print("Client request: ", data)
+    thread = threading.Thread(target=handler, args=(conn, ))
+    thread.start()
